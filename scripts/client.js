9ead2e72a5f2f90ba86ef00e2db222dee211ab9d
Kit Kogan <kkogan.dev@gmail.com>
2020-01-31 23:42:21 -0600

    update js

--- scripts/client.js
+++ scripts/client.js
@@ -10,7 +10,7 @@ function onReady() {
 
 function addEmployeeInfo() {
     console.log('in addItem');
-    //get user input and create a new item object
+    //get user input and create a new employee object
     let employee = {
         firstName: $('#firstNameIn').val(),
         lastName: $('#lastNameIn').val(),
@@ -19,5 +19,32 @@ function addEmployeeInfo() {
         annualSalary: $('#annualSalaryIn').val()
     }//end employee
     console.log('adding:', employee);
-    
+    //push employee object to emplyees array
+    employees.push(employee);
+    //empty input fields 
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idNumberIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
+    //calls the displayEmployeesInfo function
+    displayEmployeeInfo();
 }//end addItem
+
+function displayEmployeeInfo() {
+    console.log('in displayEmployeeInfo');
+    let monthlyCosts = 0
+    monthlyCosts += $(employee[i].annualSalary);
+    //select output element
+    let el = $('#infoOut');
+    //empty output element
+    el.empty();
+    //loop through array
+    for(let i = 0; i < employees.length; i++) {
+        
+        //append each item to DOM
+        el.append(`<li>${employees[i].annualSalary}</li>`);
+        //monthlyCosts +
+        console.log('monthlyCosts', monthlyCosts);
+    }//end for loop
+}//end displayEmployeeInfo
