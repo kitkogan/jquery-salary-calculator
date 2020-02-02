672b29f2779f8a4d40d40f1d5a1c845c20d33f12
Kit Kogan <kkogan.dev@gmail.com>
2020-02-02 10:27:13 -0600

    bring monthly cost additing function closer to working

--- scripts/client.js
+++ scripts/client.js
@@ -34,40 +34,62 @@ function addEmployeeInfo() {
     displayEmployeeInfo();
 }//end addItem
 
+function calculateMonthlyCosts() {
+    let monthlyCosts = 0;
+    for(let i = 0; i < employees.length; i++) {
+        //let employee = employees[i];
+        monthlyCosts += Number(employees[i].annualSalary);
+     
+    }//end if
+    el = $('.monthlyCostsTotal');
+    el.empty();
+    el.append(monthlyCosts);
+    
+    if(monthlyCosts >= 20000) {
+        $('.monthlyCostsTotal').css('background-color', 'red');
+        
+
+    }
+    console.log('monthly Cost total ', monthlyCosts);
+}//end calculateMonthly
+
 function displayEmployeeInfo() {
     console.log('in displayEmployeeInfo');
     $('.employeeRow').remove();
-    let monthlyCosts = 0;
+    //let monthlyCosts = 0;
     
     //select output element
     let el = $('#infoOut');
+    
     //empty output element
     el.empty();
     //loop through array
     for(let i = 0; i < employees.length; i++) {
         let employee = employees[i];
-        monthlyCosts += Number(employees[i].annualSalary);
+        // monthlyCosts += Number(employees[i].annualSalary);
         $('#employeeTable').append(
-            '<tr class="employeeRow">' + '<td>' + employee.firstName + '</td>' + '<td>' + employee.lastName + '</td>' + '<td>' + employee.idNumber + '</td>' + '<td>' + employee.jobTitle + '</td>' + '<td>$' + employee.annualSalary + '</td>' + '<td><button class="removeEmployeeButton">Remove Employee</button></td>' + '</tr>' + '<tr>'+ 'monthly codt' +'</tr>'+'<td class="monthlyCosts">' + monthlyCosts + '</td>'+ '</tr>');
+            '<tr class="employeeRow"><td>' + employee.firstName + '</td><td>' + employee.lastName + '</td><td>' + employee.idNumber + '</td><td>' + employee.jobTitle + '</td><td>$' + employee.annualSalary + '</td><td><button class="removeEmployeeButton">Remove Employee</button></td></tr>');
+            
         
         
         //append each employee to DOM, plus a 'remove employee' button
         //el.append(`<td>${employees[i].firstName} ${employees[i].lastName}; ID Number: ${employees[i].idNumber}, Job Title: ${employees[i].jobTitle}, Annual Salary: $${employees[i].annualSalary} <button class="removeEmployeeButton">Remove Employee</button></li>`);
         
         //monthlyCosts
-        console.log('monthly costs total: ', monthlyCosts);
+        // console.log('monthly costs total: ', monthlyCosts);
     }//end for loop
 
-    el = $('.monthlyCosts');
+    // el = $('.monthlyCostsTotal');
    
-    el.append(monthlyCosts);
-    if(monthlyCosts >= 20000) {
-        $('.monthlyCosts').css('background-color', 'red');
-        el.empty();
-    }//end if
+    // el.append(monthlyCosts);
+    // if(monthlyCosts >= 20000) {
+    //     $('.monthlyCostsTotal').css('background-color', 'red');
+    //     el.empty();
+    // }//end if
 
     //removeEmployee();
     $('.employeeRow').on('click', '.removeEmployeeButton', removeEmployee);
+    calculateMonthlyCosts();
     
 }//end displayEmployeeInfo
 
@@ -80,4 +102,6 @@ function removeEmployee() {
     
     
 }
+
+
 
