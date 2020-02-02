313c2ba9ff2d056b24e4df378f2b22ee63735d67
Kit Kogan <kkogan.dev@gmail.com>
2020-02-02 16:27:36 -0600

    readd display employee info function

--- scripts/client.js
+++ scripts/client.js
@@ -2,6 +2,9 @@ console.log('js');
  
 $(document).ready(onReady);
 
+//globals
+let employees = [];
+
 function onReady() {
    console.log('in onReady');
   $('#submitButton').on('click', addEmployeeInfo)
@@ -18,4 +21,26 @@ function addEmployeeInfo() {
         annualSalary: $('#annualSalaryIn').val()
     }//end newEmployee
     console.log('adding:', newEmployee);
+    employees.push(newEmployee);
+    displayEmployees(); //do this every time a new employee is added
+    calculateMonthlyCosts();//do this every time a new employee is added
 }//end addEmployeeInfo
+
+//calculates monthly costs and appends to the DOM each time a new employee is added
+function calculateMonthlyCosts() {
+    console.log('in calculateMonthlyCosts');
+}//end calculateMonthlyCosts
+
+//appends employee input information to DOM each time a new employee is added
+function displayEmployees() {
+    console.log('in display employees');
+    //select output element
+    let el = $('#employeeInfoOut');
+    //empty output element
+    el.empty();
+    //loop through array
+    for(let i = 0; i < employees.length; i++) {
+        //append each item to DOM
+        el.append(`<li><b>Employee Name:</b> ${employees[i].lastName}, ${employees[i].firstName}<b>; Employee ID Number:</b> ${employees[i].employeeIdNumber}<b> Employee Job Title:</b> ${employees[i].jobTitle}<b>  Annual Salary: $</b>${employees[i].annualSalary}</li>`);
+    }//end for
+}//end display employees
